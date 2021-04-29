d228abba31964771cac52c7deed6b595ecc1a97e
MAKSYM STARCHUKOV <maksym.starchukov@gmail.com>
2021-04-29 17:45:12 +0300

    splash + faq pages

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+const wrapper = () => document.querySelector('.wrapper');
+const steps = {
+  '1': (e) => {
+    wrapper().classList.add('--faq');
+  }
+}
+
+function resetStep(){
+  const w = wrapper();
+  [...w.classList].forEach(c => c.startsWith('--') && w.classList.remove(c));
+}
+
+function nextStep(e){
+  const step = e.target.closest('.btn').getAttribute('id').split('-')[1];
+  resetStep();
+  steps[step](e);
+}
+
+function setListeners(){
+  [...document.querySelectorAll('[id*="btn-"]')].forEach(b => {
+    b.addEventListener('click', nextStep, false);
+  })
+}
+
+window.onload = (e) => {
+  setListeners();
+}
